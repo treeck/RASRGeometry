d6bf82c4b19533a9520139298667a65f12abd2ee
treeck <treeck@yahoo.com>
2017-02-20 23:13:32 -0800

    Updated coq distance proof comments to more closely match article proof.

--- euclidrelations.v
+++ euclidrelations.v
@@ -537,9 +537,14 @@ Variable list_y_i: list A.
 Variables i n: nat.
 
 (**
-  A countable distance measure of a list of domain sets:
+  A countable distance measure of a list of domain sets
+  states that for each i_th disjoint domain set containing
+  p_i number of elements there exists a corresponding
+  distance set with the same p_i number of elements.
+  Notionally:
   forall i in [1,n],
-  |union X| = sum |X| and
+  |union X| = sum |X| (the intersection of domain sets is
+  an empty set) and
   cardinal of ith set of X, length list_x_i =
       cardinal ith set of Y, length list_y_i = p_i  and
   distance, d_c = Ypd = union(i=1 to n) list_y_i.
@@ -584,7 +589,9 @@ Variable p: list R.
 (** The ruler interval size. *)
 Variable c: R.
 
-(** Definitions of domain and image subintervals. *)
+(** Definitions of domain and image subintervals that are
+    used in both taxicab (Manhattan) and Euclidean distance
+    proofs. *)
 Hypothesis ruler_subintervals :
     c > 0 /\
     subintervals x_0_i x_m_i c = INR (length list_x_i) /\
@@ -597,23 +604,23 @@ Variable all_y: R.
 (** The following lemmas are steps in the proof of
    taxicab distance. *)
 
-(** Step 3.6: The largest possible set of y_i, all_y,
+(** The largest possible distance set of y_i, all_y,
     where each element, y, corresponds one-to-one to
     each x_i, is the sum of all y_i. *)
 Hypothesis all_y_sum_disjoint :
     all_y = sum_list (p).
 
-(** Step 3.8: There is one overall set of Y, Ypd number of
+(** There is one overall set of Y, Ypd number of
     subintervals. Therefore, the number of all y,
     y in Y, is (subintervals y_0 y_m c) = all_y. *)
 Hypothesis all_y_eq_image_subintervals :
     all_y = subintervals y_0 y_m c.
 
-(** The next lemma is the first proof step, 3.1, of the
-   taxicab distance proof in the article,
-   A Combinatorial Foundation for Analytic Geometry. *)
+(** The next lemma is the first proof step of the
+   taxicab and Euclidean distance proofs in the article,
+   The Real Analysis and Combinatorics of Geometry. *)
 
-(** Step 3.5: Map the set-based cardinal relationship,
+(** Map the set-based cardinal relationship,
    |{x_i}| = |{y}| = |list_y_i|, into the list of
    partition counts, p. *)
 Lemma mem_list_p_eq_list_yi :
@@ -625,7 +632,8 @@ Proof.
   assumption.
 Qed.
 
-(** Step 3.7: Multiply both sides of step 3.6 by c and
+(** Step 3.3 of taxicab distance proof:
+    Multiply both sides of step 3.2 by c and
     apply the ruler measure and covergence theorem. *)
 Lemma domain_y_measure :
     forall (L1 L2 delta epsilon:R)
@@ -660,9 +668,10 @@ Proof.
   split. assumption. assumption. assumption. 
 Qed.
 
-(** Step 3.9: Multiply both sides of step 3.8 by c and
-    apply the ruler measure and convergence theorem to get
-    the distance measure. *)
+(** Step 3.5 of taxicab distance proof:
+    Multiply both sides of step 3.4 by c and apply the
+    ruler measure and convergence theorem to get the
+    distance measure. *)
 Lemma d_measure :
     forall (L1 L2 delta epsilon:R)
         (f: R->R->R->R) (g: R->R),
@@ -689,7 +698,8 @@ Proof.
   split. assumption. assumption.
 Qed.
 
-(** Step 3.9: combine steps 3.8 and 3.6. *)
+(** The final step in the taxicab distance proof.
+    Step 3.6: Combine steps 3.5 and 3.3. *)
 Theorem taxicab_distance :
     forall (L1 L2 delta epsilon:R)
         (f: R->R->R->R) (g: R->R),
@@ -715,15 +725,16 @@ Qed.
 (** The following lemmas are steps in the proof of
    Euclidean distance. *)
 
-(** The cardinal of the set {(y,y)}. *)
-Variable all_yy: R.
+(** The cardinal of the set of combinations of the elements
+    of Y is {(y_a,y_b): y_a y_b in Y}. *)
+Variable all_yayb: R.
 
 (** The next lemma is the first proof step, 3.7, of the
    Euclidean distance proof in the article,
-   A Combinatorial Foundation for Analytic Geometry. *)
+   The Real Analysis and Combinatorics of Geometry. *)
 
-(** Step 3.13: Map the set-based cardinal relationship,
-   |{(x_i,y)}| = |{(y,y)| = |list_y_i|^2, into the list of
+(** Step 3.8: Map the set-based cardinal relationship,
+   |x_i|*|y_i| = p_i^2 = |y_i|^2, into the list of
    partition counts, p. *)
 Lemma mem_sqr_list_p_eq_prod_list_yi_list_yi :
     list_rmem i (sqr_list p) = Rsqr (subintervals x_0_i x_m_i c).
@@ -734,15 +745,15 @@ Proof.
   rewrite -> sqr_list_spec in H10. assumption.
 Qed.
 
-(** Step 3.12: The largest possible set of (y,y), all_yy,
-    where each combinatiion, (y,y), corresponds one-to-one to
-    each (x_i,y_i), is the sum of all (y_i,y_i). *)
-Hypothesis all_yy_sum_disjoint :
-    all_yy = sum_list (sqr_list p).
+(** Step 3.10: Choose the equality case of the relation
+    sum_list(sqr_list p) >=
+        |{(y_a,y_b): y_a y_b in Y}| = all_yayb *)
+Hypothesis all_yayb_sum_disjoint :
+    all_yayb = sum_list (sqr_list p).
 
-(** Step 3.14: Multiply both sides of step 3.13 by Rsqr c and
+(** Step 3.11: Multiply both sides of step 3.10 by Rsqr c and
     apply the ruler measure and covergence theorem. *)
-Lemma domain_yy_measure :
+Lemma domain_yayb_measure :
     forall (L1 L2 delta epsilon:R)
         (f: R->R->R->R) (g: R->R),
     c > 0 /\
@@ -750,14 +761,14 @@ Lemma domain_yy_measure :
     L1 = 0 /\
     0 < delta /\ 0 < epsilon /\ delta = epsilon /\
     0 < Rabs (c - L1) < delta ->
-    all_yy * Rsqr c = sum_list (sqr_list s).
+    all_yayb * Rsqr c = sum_list (sqr_list s).
 Proof.
   intros. decompose [and] H.
   decompose [and] exact_sizes. decompose [and] ruler_subintervals.
-  assert (all_yy * Rsqr c = sum_list (mult_list (sqr_list p) c²)).
+  assert (all_yayb * Rsqr c = sum_list (mult_list (sqr_list p) c²)).
       rewrite <- mult_distributes_over_sum_list.
       apply Rmult_eq_compat_r with (r := Rsqr (c))
-          (r1 := all_yy)
+          (r1 := all_yayb)
           (r2 := sum_list (sqr_list p)).
       assumption.
   rewrite <- limit_c_0_M_eq_exact_size with
@@ -786,13 +797,13 @@ Proof.
   split. assumption. assumption. 
 Qed.
 
-(** Step 3.15: There is one overall set of Y, Ypd number of
+(** Step 3.12: There is one overall set of Y, Ypd number of
     subintervals. Therefore, the number of all (y,y) combinations,
-    y in Y, is (subintervals y_0 y_m c)^2 = all_yy. *)
-Hypothesis all_yy_eq_rsqr_image_subintervals :
-    all_yy = Rsqr (subintervals y_0 y_m c).
+    y in Y, is (subintervals y_0 y_m c)^2 = all_yayb. *)
+Hypothesis all_yayb_eq_rsqr_image_subintervals :
+    all_yayb = Rsqr (subintervals y_0 y_m c).
 
-(** Step 3.16: Multiply both sides of step 3.7 by Rsqr c and
+(** Step 3.13: Multiply both sides of step 3.12 by Rsqr c and
     apply the ruler measure and convergence theorem to get
     the distance measure. *)
 Lemma rsqr_d_measure :
@@ -803,7 +814,7 @@ Lemma rsqr_d_measure :
     L1 = 0 /\
     0 < delta /\ 0 < epsilon /\ delta = epsilon /\
     0 < Rabs (c - L1) < delta ->
-    Rsqr d = all_yy * Rsqr c.
+    Rsqr d = all_yayb * Rsqr c.
 Proof.
   intros. decompose [and] H.
   decompose [and] exact_sizes. decompose [and] ruler_subintervals.
@@ -816,15 +827,15 @@ Proof.
   rewrite <- rsqr_distributes.
   apply Rmult_eq_compat_r with (r := Rsqr (c))
       (r1 := Rsqr (subintervals y_0 y_m c))
-      (r2 := all_yy).
-  symmetry. apply all_yy_eq_rsqr_image_subintervals.
+      (r2 := all_yayb).
+  symmetry. apply all_yayb_eq_rsqr_image_subintervals.
   split. assumption. split. reflexivity. split. assumption.
   split. assumption. split. reflexivity.
   split. assumption. split. assumption. split. assumption.
   split. assumption. assumption.
 Qed.
 
-(** Step 3.17: combine steps 3.15 and 3.16. *)
+(** Step 3.14: combine steps 3.11 and 3.13. *)
 Theorem Euclidean_distance :
     forall (L1 L2 delta epsilon:R)
         (f: R->R->R->R) (g: R->R),
@@ -837,10 +848,10 @@ Theorem Euclidean_distance :
 Proof.
   intros. decompose [and] H.
   decompose [and] exact_sizes. decompose [and] ruler_subintervals.
-  assert (all_yy * Rsqr c = sum_list (sqr_list s)).
+  assert (all_yayb * Rsqr c = sum_list (sqr_list s)).
       revert H.
-      apply domain_yy_measure. assumption.
-  assert (Rsqr d = all_yy * Rsqr c).
+      apply domain_yayb_measure. assumption.
+  assert (Rsqr d = all_yayb * Rsqr c).
       revert H.
       apply rsqr_d_measure. assumption.
   rewrite -> H14 in H16.
